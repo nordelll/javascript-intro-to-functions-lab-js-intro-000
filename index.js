964d50a299f9adad42c8b2bf8d9fb8d40dffe349
L Nordell <lnordell@gmail.com>
2018-07-25 18:42:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  if (string === string.toUpperCase()) {
-    console.log(string);
-  }
+  //if (string === string.toUpperCase()) {
+    console.log(string.toUpperCase());
+  //}
 }
